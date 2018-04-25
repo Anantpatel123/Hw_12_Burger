81a66263ef1fd4956c2b379c2f798e0a4a164cec
Anant Patel <Anant1234@hotmail.com>
2018-04-25 19:45:56 -0400

    final changes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -27,52 +27,9 @@ router.post('/api/burgers', function(req, res) {
 router.put('/api/update/:id', function(req, res) {
 	var condition = 'id = ' + req.params.id;
 	console.log('condition', condition);
-
-	burger.update({devoured: req.body.devoured}, condition, function() {
-		// if (result.changedRows == 0) {
-		// 	// If no rows were changed, then the ID must not exist, so 404
-		// 	return res.status(404).end();
-		//   } else {
-		// 	res.status(200).end();
-		//   }
+	burger.update({devoured: req.body.devoured}, condition, function() {	
 		res.redirect('/index');
 	});
 });
-
-
-
-// router.get("/index", function(req, res) {
-//   burger.all(function(data) {
-//     var hbsObject = {
-//       burger: data
-//     };
-//     // console.log("this is hbsobject ", hbsObject);
-//     res.render("index", hbsObject);
-//   });    
-// });
-// router.post("/api/burgers", function(req, res) {
-//   // console.log(req.body);
-//   burger.create(["burger_name", "devoured"], [req.body.burger_name, false], function(result) {
-//     // Send back the ID of the new quote
-//     console.log("added burger: ", result)
-//     res.json({ id: result.insertId });
-    
-//     });
-// });
-
-// router.put("/api/update/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;  
-//   console.log("condition", condition);  
-//   burger.update({
-//     devoured: req.body.devoured
-//   }, condition, function() {
-//     res.redirect("/index")
-//     if (result.changedRows == 0) {
-//         return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
 
 module.exports = router;
